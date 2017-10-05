9176862c87c725fd2ebdc499e4d10c39a77d5231
Ilya V. Azarov <brainstorm@brainstorm.name>
2017-10-05 10:31:31 +0300

    format fix

--- examples/example.go
+++ examples/example.go
@@ -1,16 +1,17 @@
 package main
-import(
-  "fmt"
-  "github.com/Ilya1st/configuration-go"
+
+import (
+	"fmt"
+	"github.com/Ilya1st/configuration-go"
 )
 
-func main(){
-  config, err:=configuration.GetConfigInstance("mainconfig", "HJSON", "test.hjson")
-  if err != nil {
-    fmt.Printf("Error occured %v\n", err);
-  }
-  val, err:=config.GetValue("section1", "subsection2", "value")
-  fmt.Printf("getting section1/subsection2/value: val=%v, error=%v\n", val, err)
-  val, err=config.GetValue("test")
-  fmt.Printf("getting test: val=%v, error=%v\n", val, err)
+func main() {
+	config, err := configuration.GetConfigInstance("mainconfig", "HJSON", "test.hjson")
+	if err != nil {
+		fmt.Printf("Error occured %v\n", err)
+	}
+	val, err := config.GetValue("section1", "subsection2", "value")
+	fmt.Printf("getting section1/subsection2/value: val=%v, error=%v\n", val, err)
+	val, err = config.GetValue("test")
+	fmt.Printf("getting test: val=%v, error=%v\n", val, err)
 }
